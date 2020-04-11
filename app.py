f9a75fa5c872692793e2183a98f921464711143a
Ubuntu <ubuntu@ip-172-31-38-241.ec2.internal>
2020-04-11 13:48:37 +0000

    added insert_task functionality

--- app.py
+++ app.py
@@ -10,12 +10,22 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:MongoMongo1990@myfirstcluster-sdcs
 
 mongo = PyMongo(app)
 
-
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
     # Renders the tasks.html file and uses .find() to return everything in the tasks collection
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+@app.route('/add_task')
+def add_task():
+    return render_template("addtask.html", categories=mongo.db.categories.find())
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    # Inserts the request object into the tasks collection. We also convert the form to a dictionary
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
